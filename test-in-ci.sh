df9032176d70b27fabec3dd33966452eb5da8a44
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-03-01 01:14:07 -0500

    Try with maturin develop

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -4,6 +4,5 @@ set -e
 
 hash uv || python3 -m pip install uv
 uv venv --python=3.12 --allow-existing
-uv sync --no-build --no-install-project
-uv pip install dist/*.whl
+uv run maturin develop
 uv run pytest -vvs
