455ac76154c15249835e26fb239cacc9806ddff2
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-03-01 02:08:29 -0500

    Extract wheel manually to add to PYTHONPATH

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -7,5 +7,6 @@ export RUSTC_WRAPPER=
 hash uv || python3 -m pip install uv
 uv venv --python=3.12 --allow-existing
 uv sync --no-build --no-install-project
-uv pip install dist/scors-*cp312-cp312*whl
-uv run pytest -vvs
+mkdir -p tmp_path
+(cd tmp_path && unzip ../dist/scors*cp312-cp312*whl)
+PYTHONPATH=./tmp_path uv run --no-project pytest -vvs
