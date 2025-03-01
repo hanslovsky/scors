050254076342d9f25b1c539a2f1562fade7d2454
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-03-01 01:08:57 -0500

    Try installing pre-built wheel

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -4,5 +4,6 @@ set -e
 
 hash uv || python3 -m pip install uv
 uv venv --python=3.12 --allow-existing
-uv sync --no-editable
+uv sync --no-build --no-install-project
+uv pip install dist/*.whl
 uv run pytest -vvs
