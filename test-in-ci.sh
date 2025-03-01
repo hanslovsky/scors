7fee5706611ea291f856365f7e02d2b6751c36ca
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-03-01 01:33:42 -0500

    Try using pre-build wheels

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -6,5 +6,6 @@ export SCCACHE_GHA_ENABLED=false
 export RUSTC_WRAPPER=
 hash uv || python3 -m pip install uv
 uv venv --python=3.12 --allow-existing
-uv sync
+uv sync --no-build --no-install-project
+uv pip install dist/scors-*cp312-cp312*whl
 uv run pytest -vvs
