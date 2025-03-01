8e5fd6277330f781039ae13a2cb0f014e2e13ec4
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-03-01 01:26:43 -0500

    Revert to uv sync

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -6,5 +6,5 @@ export SCCACHE_GHA_ENABLED=false
 export RUSTC_WRAPPER=
 hash uv || python3 -m pip install uv
 uv venv --python=3.12 --allow-existing
-uv run maturin develop
+uv sync
 uv run pytest -vvs
