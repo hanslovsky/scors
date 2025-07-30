adc71c669cd84790155a6e7986ae29a933a9a162
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-07-29 20:01:40 -0400

    Update roc_auc to new generic code

--- src/scors/__init__.py
+++ src/scors/__init__.py
@@ -1,16 +1,66 @@
 import logging
+from dataclasses import dataclass
+from typing import Callable
 
 import numpy as np
 
 from . import _scors as scors
 from ._scors import *
 
-__doc__ = _scors.__doc__
+__doc__ = scors.__doc__
 
 
 _logger = logging.getLogger("scors")
 
+_supported_label_types = (
+    ("bool", "bool",),
+    ("int8", "i8",),
+    ("int16", "i16"),
+    ("int32", "i32"),
+    ("int64", "i64"),
+    ("uint8", "u8",),
+    ("uint16", "u16"),
+    ("uint32", "u32"),
+    ("uint64", "u64"),
+)
+_supported_score_types = (
+    ("float32", "f32"),
+    ("float64", "f64"),
+)
 
+def _lookup_supported_type(dtype: str | np.dtype, supported_type_dict: dict[str, str]) -> str:
+    dtype_name = dtype if isinstance(dtype, str) else dtype.name
+    try:
+        return supported_type_dict[dtype_name]
+    except KeyError as e:
+        raise TypeError(f"Unsupported {dtype=} ({dtype_name=}). Supported types: {tuple(supported_type_dict.keys())}")
+
+
+def _lookup_supported_label_type(dtype: str | np.dtype) -> str:
+    return _lookup_supported_type(dtype, dict(_supported_label_types))
+
+
+def _lookup_supported_score_type(dtype: str | np.dtype) -> str:
+    return _lookup_supported_type(dtype, dict(_supported_score_types))
+
+def _lookup_supported_score_func(
+        func_name: str,
+        label_dtype: str | np.dtype,
+        score_dtype: np.dtype | str
+) -> Callable[[np.ndarray, np.ndarray, np.ndarray | None], float]:
+    specific_func_name = f"{func_name}_{_lookup_supported_label_type(label_dtype)}_{_lookup_supported_score_type(score_dtype)}"
+    return getattr(scors, specific_func_name)
+
+
+def _from_generic_score(scors_name: str) -> Callable[[np.ndarray, np.ndarray, np.ndarray | None, Order | None], float]:
+    def scors_func(labels: np.ndarray, predictions: np.ndarray, *args, weights: np.ndarray | None = None, order: Order | None = None, **kwargs):
+        if weights is not None and predictions.dtype != weights.dtype:
+            raise ValueError(f"Weights must be the same dtype as predictions, if provided, but found {weights.dtype=} != {predictions.dtype=}")
+        return _lookup_supported_score_func(scors_name, labels.dtype, predictions.dtype)(labels, predictions, *args, weights=weights, order=order, **kwargs)
+    return scors_func
+        
+
+    
 def _loo_cossim_many(data: np.ndarray):
     if data.dtype == np.float32:
         return loo_cossim_many_f32(data)
@@ -23,6 +73,14 @@ def loo_cossim_many(data: np.ndarray):
     sim = _loo_cossim_many(np.reshape(data, (-1, *data.shape[-2:])))
     sim_reshaped = np.reshape(sim, data.shape[:-2])
     return sim_reshaped
+
+
+def average_precision(labels: np.ndarray, predictions: np.ndarray, weights: np.ndarray | None = None, order: Order | None = None):
+    return _from_generic_score("average_precision")(labels=labels, predictions=predictions, weights=weights, order=order)
+
+
+def roc_auc(labels: np.ndarray, predictions: np.ndarray, weights: np.ndarray | None = None, order: Order | None = None, max_fpr: float | None = None):
+    return _from_generic_score("roc_auc")(labels=labels, predictions=predictions, weights=weights, order=order, max_fpr=max_fpr)
 
 
 average_precision_on_two_sorted_samples_deprecated = average_precision_on_two_sorted_samples
@@ -51,16 +109,8 @@ def average_precision_on_two_sorted_samples(
     if w2 is not None and w2.dtype != p2.dtype:
         raise TypeError(f"Weight array must have the same dtype as predictions but found: {w2.dtype=} != {p2.dtype=}")
 
-    supported_label_types = ("bool", "int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64")
-    supported_score_types = ("float32", "float64")
-
-    if l1.dtype.name not in supported_label_types:
-        raise TypeError(f"Unsupported type for labels: {l1.dtype}. Supported types: {supported_label_types}")
-    l_dtype = l1.dtype.name.replace("uint", "u").replace("int", "i")
-
-    if w1.dtype.name not in supported_score_types:
-        raise TypeError(f"Unsupported type for predictions/weights: {w1.dtype}. Supported types: {supported_score_types}")
-    p_dtype = p1.dtype.name.replace("float", "f")
+    l_dtype = _lookup_supported_label_type(l1.dtype)
+    p_dtype = _lookup_supported_score_type(p1.dtype)
 
     func_name = f"average_precision_on_two_sorted_samples_{l_dtype}_{p_dtype}"
     _logger.info(f"{func_name=}")
@@ -69,8 +119,9 @@ def average_precision_on_two_sorted_samples(
     
 
 
-__all__ = [
+__all__ = sorted([
+    "average_precision",
     "average_precision_on_two_sorted_samples",
     "loo_cossim_many",
-    scors.__all__
-]
+    *scors.__all__
+])
