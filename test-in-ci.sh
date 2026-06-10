088f1aa5da8e82fdddcc982a4331d71e3ae2dc66
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2026-06-10 18:49:55 -0400

    Bump pyo3 and python interpreter versions

--- test-in-ci.sh
+++ test-in-ci.sh
@@ -5,8 +5,9 @@ set -e
 export SCCACHE_GHA_ENABLED=false
 export RUSTC_WRAPPER=
 hash uv || python3 -m pip install uv
-uv venv --python=3.12 --allow-existing
+uv venv --allow-existing
 uv sync --no-build --no-install-project
 mkdir -p tmp_path
-(cd tmp_path && unzip ../dist/scors*cp312-cp312*whl)
+PYVER=$(python3 -c "import sys; print(f'cp{sys.version_info.major}{sys.version_info.minor}')")
+(cd tmp_path && unzip ../dist/scors*${PYVER}*.whl)
 PYTHONPATH=./tmp_path uv run --no-project pytest -vvs
